c43e052382b70b02f3e69e952e53319cc1f7b6a0
grizzly2609 <elia.grizzant@gmail.com>
2024-12-27 14:18:56 +0100

    primo sito

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+
+    document.querySelector('.logo').addEventListener('click', () => {
+        window.location.href = 'index.html';
+    });
+});
